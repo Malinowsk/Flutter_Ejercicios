a852d4ab718f1ac62a73ee22ed08e339b9e2601f
Malinowsk <juanrago2015@gmail.com>
2024-05-17 19:07:14 -0300

    Proyecto SpeedDial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   int _counter = 0;
+  var _layout=MainAxisAlignment.center;
 
   void _incrementCounter() {
     setState(() {
@@ -39,32 +42,75 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _nextLayout() {
+    setState(() {
+      _layout=_nextEnumLayout(_layout);
+    });
+  }
+
+  MainAxisAlignment _nextEnumLayout(MainAxisAlignment value) {
+    final nextIndex = (value.index + 1) % MainAxisAlignment.values.length;
+    return MainAxisAlignment.values[nextIndex];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar( // header  
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+      
       body: Center(
+        
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: _layout,
+          
           children: <Widget>[
+            
             const Text(
               'You have pushed the button this many times:',
             ),
+            
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            
+            const Text('Current Layout:', ),
+            
+            Text('$_layout',
+              style: Theme.of(context).textTheme.headlineMedium, 
+            ),
+          
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+    floatingActionButton: SpeedDial(
+      icon: Icons.star,
+      backgroundColor: Colors.blue,
+      children: [
+
+          SpeedDialChild(
+            child: const Icon(Icons.plus_one,color: Colors.white),
+            label: 'Increment',
+            backgroundColor: Colors.blueAccent,
+            onTap: _incrementCounter,
+          ),
+        
+          SpeedDialChild(
+            child: const Icon(Icons.layers_outlined,color: Colors.white),
+            label: 'Layout',
+            backgroundColor: Colors.blueAccent,
+            onTap: _nextLayout,
+          ),
+
+        ]
+
+      ),
+
     );
   }
 }
